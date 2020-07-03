145f4982b02e86f019ba9b870b2c6940fa726b53
jeremy wambugu <wambugujeremy@gmail.com>
2020-07-03 09:43:49 +0300

    Updates

---
+++ precache-manifest.18d89718d31781f6b3e394c3b265f1c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc61fb27683577a00f912e3467c3ff77",
+    "url": "/Breaking-Bad-Searcher/index.html"
+  },
+  {
+    "revision": "ea13f93fd20d31cc5239",
+    "url": "/Breaking-Bad-Searcher/static/css/main.d46e63f3.chunk.css"
+  },
+  {
+    "revision": "82ff9f4bdf28a6f26d57",
+    "url": "/Breaking-Bad-Searcher/static/js/2.2cfd02f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-Searcher/static/js/2.2cfd02f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea13f93fd20d31cc5239",
+    "url": "/Breaking-Bad-Searcher/static/js/main.716587b6.chunk.js"
+  },
+  {
+    "revision": "2cfa29f2dda951755cfe",
+    "url": "/Breaking-Bad-Searcher/static/js/runtime-main.259bf290.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-Searcher/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-Searcher/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-Searcher/static/media/spinner.235b9b3e.gif"
+  }
+]);
